36f07c5bb57a41ee0d917972760194d1ce71a4a9
Vitaliy Sekan <v.siekan@svitla.com>
2020-01-13 18:51:37 +0200

    Checkings added and small changes

---
+++ README.md
@@ -0,0 +1,10 @@
+# Solution for the paint shop problem.
+
+Requirements:
+ * PHP (any version);
+ * PHP Cli;   
+ 
+How to use example:
+``
+php paint.php -f input_data/data4.in 
+`` 
--- paint.php
+++ paint.php
@@ -1,14 +1,8 @@
 <?php
 
-//print phpinfo();
-//die();
-
-
 $shop = new PaintShop();
-$shop->loadPreferences('data.in');
-$shop->processData();
+
 $shop->calculateColors();
-//$shop->printPreferences();
 $shop->printColors();
 
 
@@ -23,137 +17,104 @@ class PaintShop {
 
   private $colorResult;
 
-
-  private function removeColorsFromUsers($colorNumber,  $colorStyle) {
-
-    $color = $colorNumber . $colorStyle;
-    $oppositeColor = $colorNumber . ($colorStyle == 'G' ? 'M' : 'G');
-
-    foreach ($this->preferences as $key => $userPreference) {
-      // successed users.
-      if (in_array($color, $userPreference['pref'])) {
-        unset($this->preferences[$key]);
-      }
-
-      // remove color variant from user
-      if (in_array($oppositeColor, $userPreference['pref'])) {
-        $keyToRemove = array_search($oppositeColor, $userPreference['pref']);
-        
-        unset($this->preferences[$key]['pref'][$keyToRemove]);
-        $this->preferences[$key]['variants']--;
-      }
-
-    }
+  public function __construct() {
+    $this->loadPreferences();
+    $this->processData();
   }
+
   /**
-   * @return mixed
+   * Load preferences file.
    */
+  private function loadPreferences() {
+    // Get input file name from '-f' param.
+    $params = getopt("f:");
+    $fileName = $params['f'];
 
-  private function getVariants($colorNumber) {
-    $v = ['color' => 'G', 'users'];
-    $lastG = FALSE;
-    foreach ($this->preferences as $key => $userPreference) {
+    try {
+      if (!file_exists($fileName)) {
+        throw new Exception('File ' . $fileName . ' doest exist');
+      }
+    } catch (Exception $e) {
+      echo 'Error: ' . $e->getMessage() . "\n";
+      exit;
+    }
 
-      // first user preference.
-      $pref = reset($userPreference['pref']);
+    $this->fileContent = file_get_contents($fileName);
+  }
 
-      if ((integer) $pref == $colorNumber) {
-        $v['users'][] = $key;
+  /**
+   * Process user preferences.
+   */
+  private function processData() {
+    $rows = explode("\n", $this->fileContent);
 
-        if ($userPreference['variants'] == 1) {
-          $lastG = TRUE;
-        }
+    try {
+      if (intval($rows[0]) < 1) {
+        throw new Exception('Colors count should be positive integer');
+      }
 
-        if (substr($pref, 1, 1) !== 'G' && $userPreference['variants'] == 1) {
-          $v['color'] = 'M';
+      // Set colors count.
+      $this->colorsCount = $rows[0];
 
-          if ($lastG) {
-            return FALSE;
+      // Get and check preferences.
+      for ($i = 1; $i < count($rows); $i++) {
+        $line = explode(' ', $rows[$i]);
+        $mCount = 0;
+
+        for ($j = 0; $j < count($line); $j = $j + 2) {
+          // Check color number.
+          $colorNumber = $line[$j];
+          $colorStyle = $line[$j + 1];
+
+          if (intval($colorNumber) < 1 || $colorNumber > $this->colorsCount) {
+            throw new Exception('Please check color numbers for user ' . $i);
+          }
+
+          if ($colorStyle === 'M' || $colorStyle === 'G') {
+            if ($colorStyle == 'M') {
+              $mCount++;
+            }
+
+            if ($mCount > 1) {
+              throw new Exception('More than 1 matte color for user ' . $i);
+            }
+
+            $this->preferences[$i]['pref'][] = $colorNumber . $colorStyle;
+          }
+          else {
+            throw new Exception('Please check color styles for user ' . $i);
           }
         }
 
+        $this->preferences[$i]['variants'] = count($this->preferences[$i]['pref']);
 
-        // TODO: check for last variant G and M.
       }
-
-
-
-    }
-
-    return $v;
-  }
-
-  public function calculateColors() {
-
-    $i=1;
-
-    while ($i < $this->colorsCount + 1) {
-      // get variants. No variants, G or M, or conflict.
-      $v = $this->getVariants($i);
-
-      if (!$v) {
-        $this->solutionExists = FALSE;
-        break;
-      }
-
-      // set color.
-      $this->colorResult[$i] = $v['color'];
-
-      $this->removeColorsFromUsers($i,  $v['color']);
-      $i++;
-
-    }
-
-
-
-
-    // Fill empty colors.
-    $i = 1;
-    while ($i < $this->colorsCount) {
-      if (!isset($this->colorResult[$i])) {
-        $this->colorResult[$i] = "G";
-      }
-      $i++;
-    }
-
-  }
-
-  public function loadPreferences($fileName) {
-    // TODO add file checkings.
-    $this->fileContent = file_get_contents($fileName);
-
-  }
-
-  public function processData() {
-    $rows = explode("\n", $this->fileContent);
-
-    $this->colorsCount = $rows[0];
-
-    for ($i = 1; $i < count($rows); $i++) {
-      $line = explode(' ', $rows[$i]);
-
-      for ($j = 0; $j < count($line); $j = $j + 2) {
-        $this->preferences[$i]['pref'][] = $line[$j] . $line[$j+1];
-      }
-
-      $this->preferences[$i]['variants'] = count($this->preferences[$i]['pref']);
-
+    } catch (Exception $e) {
+      echo 'Error: ' . $e->getMessage() . "\n";
+      exit;
     }
 
     // Sort data.
     $this->preferencesSort();
-
   }
 
+  /**
+   * Change user order to have minimum preferences at the top.
+   */
   private function preferencesSort() {
     // Sort users by preferences count.
     usort($this->preferences, function ($x, $y) {
-      if ($x['variants'] == $y['variants'])
+      if ($x['variants'] == $y['variants']) {
         return 0;
-      else if ($x['variants'] > $y['variants'])
-        return 1;
-      else
-        return -1;
+      }
+      else {
+        if ($x['variants'] > $y['variants']) {
+          return 1;
+        }
+        else {
+          return -1;
+        }
+      }
     });
 
     // Sort preferences for each user by color.
@@ -162,20 +123,144 @@ class PaintShop {
         $colorX = (integer) $x;
         $colorY = (integer) $y;
 
-        if ($colorX == $colorY)
+        if ($colorX == $colorY) {
           return 0;
-        else if ($colorX > $colorY)
-          return 1;
-        else
-          return -1;
+        }
+        else {
+          if ($colorX > $colorY) {
+            return 1;
+          }
+          else {
+            return -1;
+          }
+        }
       });
 
       $this->preferences[$key]['pref'] = $userPreference['pref'];
     }
-
-
   }
 
+  /**
+   * Set order how colors should be processed.
+   * @return array ordered colors list.
+   */
+  private function initColors() {
+    $colorsOrder = [];
+    // Set colors order from users with 1 variant.
+    // Get all users with 1 variant and put color number at the beginning.
+    foreach ($this->preferences as $userPreference) {
+      if ($userPreference['variants'] == 1) {
+        // store color.
+        $colorNumber = (integer) $userPreference['pref'][0];
+
+        if (!in_array($colorNumber, $colorsOrder)) {
+          $colorsOrder[] = $colorNumber;
+        }
+      }
+    }
+
+    // Fill all colors order.
+    $i = 1;
+
+    while ($i < $this->colorsCount + 1) {
+      if (!in_array($i, $colorsOrder)) {
+        $colorsOrder[] = $i;
+      }
+      $i++;
+    }
+
+    return $colorsOrder;
+  }
+
+  /**
+   * Colors processing to get results.
+   */
+  public function calculateColors() {
+    $colorsOrder = $this->initColors();
+
+    // Check all colors and set result for each of them.
+    foreach ($colorsOrder as $i) {
+      $colorStyle = $this->getVariants($i);
+
+      if (!$colorStyle) {
+        $this->solutionExists = FALSE;
+        return;
+      }
+
+      // Set color.
+      $this->colorResult[$i] = $colorStyle;
+
+      // Remove satisfied users and remove color even it doesn't match.
+      $this->removeColorsFromUsers($i, $colorStyle);
+    }
+  }
+
+  /**
+   * Removes color number from user preferences or removes satisfied user.
+   *
+   * @param $colorNumber integer
+   * @param $colorStyle string
+   */
+  private function removeColorsFromUsers($colorNumber, $colorStyle) {
+
+    $color = $colorNumber . $colorStyle;
+    $oppositeColor = $colorNumber . ($colorStyle == 'G' ? 'M' : 'G');
+
+    foreach ($this->preferences as $key => $userPreference) {
+      // Satisfied user.
+      if (in_array($color, $userPreference['pref'])) {
+        unset($this->preferences[$key]);
+      }
+
+      // Remove color variant from user.
+      if (in_array($oppositeColor, $userPreference['pref'])) {
+        $keyToRemove = array_search($oppositeColor, $userPreference['pref']);
+
+        unset($this->preferences[$key]['pref'][$keyToRemove]);
+        $this->preferences[$key]['variants']--;
+      }
+    }
+  }
+
+  /**
+   *  Returns color style for color number.
+   *
+   * @param $colorNumber integer
+   * @return bool|string false if not variant and M or G.
+   */
+  private function getVariants($colorNumber) {
+    $colorStyle = 'G';
+    $lastG = $lastM = FALSE;
+
+    foreach ($this->preferences as $key => $userPreference) {
+
+      // first user preference.
+      $pref = reset($userPreference['pref']);
+
+      if ((integer) $pref == $colorNumber) {
+        $userColor = substr($pref, 1, 1);
+
+        if ($userPreference['variants'] == 1 && $userColor == 'G') {
+          $lastG = TRUE;
+        }
+
+        if ($userColor !== 'G' && $userPreference['variants'] == 1) {
+          $colorStyle = 'M';
+          $lastM = TRUE;
+        }
+      }
+
+      if ($lastM && $lastG) {
+        return FALSE;
+      }
+    }
+
+    return $colorStyle;
+  }
+
+  /**
+   * Print user preferences.
+   */
   public function printPreferences() {
     foreach ($this->preferences as $userPreference) {
       print $userPreference['variants'] . ' ';
@@ -185,6 +270,9 @@ class PaintShop {
     }
   }
 
+  /**
+   * Print results.
+   */
   public function printColors() {
     if (!$this->solutionExists) {
       print 'No solution exists';
